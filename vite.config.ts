4e899833b4cd5c4a858b24c0d4384ea2aa025337
Ashna Susan Saji <ashna.ss030@gmail.com>
2025-02-11 16:41:08 +0000

    fix built files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/',  // Add this line with your GitHub repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
